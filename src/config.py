83a0a3cfd767aea9fcb19a64c81d188e5ebd58f5
Allan Henrique <allanmoura2211@outlook.com>
2026-05-04 19:47:29 -0300

    adicionando configs

--- src/config.py
+++ src/config.py
@@ -7,5 +7,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 NASA_API_KEY = os.getenv("NASA_API_KEY", 'DEMO_KEY')
-NASA_API_KEY = os.getenv("NASA_API_KEY", 'DEMO_KEY')
-NASA_API_KEY = os.getenv("NASA_API_KEY", 'DEMO_KEY')
+START_DATE = os.getenv("START_DATE", '2026-04-01')
+END_DATE = os.getenv("END_DATE", '2026-04-10')
+
+RAW_OUTPUT = BASE_DIR / os.getenv("RAW_OUTPUT","data/raw/apod.json")
+PROCESSED_OUTPUT = BASE_DIR / os.getenv("PROCESSED_OUTPUT","data/processed/apod_cleaned")
+CURATED_OUTPUT = BASE_DIR / os.getenv("CURATED_OUTPUT","data/curated/apod_metrics")
